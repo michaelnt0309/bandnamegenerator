4615be80165475fff276000b4cd6cc21d9918550
michaelnt0309 <156021625+michaelnt0309@users.noreply.github.com>
2024-01-08 12:48:19 -0800

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,5 @@
+print("Greetings!")
+city = input("What city that you grew up in:\n")
+pet_name = input("What is your pet's name:\n")
+band_name = city + " " + pet_name
+print("Your band name is " + band_name)
